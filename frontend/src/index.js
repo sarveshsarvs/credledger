582e09260620d830dfd3354f63bfe4398b1c05b3
sarvesh <sarveshjrgar@gmail.com>
2025-09-29 15:05:30 +0530

    Test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import Demo from './Demo';
+import App from './App';
+//import Demo from './Demo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Demo />
+    <App />
   </React.StrictMode>
 );
 
